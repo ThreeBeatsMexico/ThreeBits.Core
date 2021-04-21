04a15a00906b0cd9ec1ace4d4701359e483a7bdf
Bryan-Tinajero <81982094+Bryan-Tinajero@users.noreply.github.com>
2021-04-20 21:30:04 -0500

    Entidades
    Se crearon las entidades de la escuela

---
+++ ThreeBits.Entities/School/AlumnosBE.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeBits.Entities.School
+{
+    class AlumnosBE
+    {
+        public string sIdAlumno { get; set; }
+        public string sNumeroMatricula { get; set; }
+        public string sFechaRegistro { get; set; }
+        public string sFechaMatricula { get; set; }
+        public string sAPaterno { get; set; }
+        public string sAMaterno { get; set; }
+        public string sNombres { get; set; }
+        public string sFechaNacimiento { get; set; }
+        public string sSexo { get; set; }
+        public string sNacionalidad { get; set; }
+        public string sGrado { get; set; }
+        public string sGrupo { get; set; }
+        public string sNumeroLista { get; set; }
+        public string sEscuelaProcedencia { get; set; }
+        public string sHermanos { get; set; }
+        public string sGradoHermanos { get; set; }
+        public string sCalle { get; set; }
+        public string sNumero { get; set; }
+        public string sColonia { get; set; }
+        public string sDelegacion { get; set; }
+        public string sEstado { get; set; }
+        public string sCodigoPostal { get; set; }
+        public string sTelefono { get; set; }
+        public string sEmail { get; set; }
+        public string sCurp { get; set; }
+        public string sEdadAnos { get; set; }
+        public string sEdadMeses { get; set; }
+        public string sNombrePadre { get; set; }
+        public string sEstatus { get; set; }
+        public string sNombrePadreTutor { get; set; }
+        public string sOcupacionPadre { get; set; }
+        public string sTelefonoPadre { get; set; }
+        public string sTelefonoTrabajoPadre { get; set; }
+        public string sCelularPadre { get; set; }
+        public string sFechaNacimientoPadre { get; set; }
+        public string sSueldoPadre { get; set; }
+        public string sNacionalidadPadre { get; set; }
+        public string sNombreMadreTutor { get; set; }
+        public string sOcupacionMadre { get; set; }
+        public string sTelefonoMadre { get; set; }
+        public string sTelefonoTrabajoMadre { get; set; }
+        public string sCelularMadre { get; set; }
+        public string sFechaNacimientoMadre { get; set; }
+        public string sSueldoMadre { get; set; }
+        public string sNacionalidadMadre { get; set; }
+        public string sNombreFamVecino { get; set; }
+        public string sTelefonoVecino { get; set; }
+        public string sTelefonoTrabajoVecino { get; set; }
+        public string sCelularVecino { get; set; }
+        public string sEducacionFisica { get; set; }
+        public string sMedicamento { get; set; }
+        public string sNombreMedicamento { get; set; }
+        public string sDosisMedicamento { get; set; }
+        public string sAlimentoProhibido { get; set; }
+        public string sPeso { get; set; }
+        public string sTalla { get; set; }
+        public string sTipoSangre { get; set; }
+        public string sEnfermedades { get; set; }
+        public string sNombreEnfermedades { get; set; }
+        public string sProcedimientoCrisis { get; set; }
+        public string sCertificado { get; set; }
+        public string sEnfermedadCertificado { get; set; }
+        public string sAlergia { get; set; }
+        public string sNombreAlergia { get; set; }
+        public string sProcedimintoCrisisAlergia { get; set; }
+        public string sNombreAccidente { get; set; }
+        public string sTelefonoAccidente { get; set; }
+        public string sNombreHospital { get; set; }
+        public string sMedico { get; set; }
+        public string sNombreMedico { get; set; }
+        public string sTelefonoMedico { get; set; }
+        public string sCedulaMedico { get; set; }
+        public string sAutorizaTraslado { get; set; }
+        public string sProcedimientoAccidente { get; set; }
+
+        public string sFoto { get; set; }
+        public string sNivelAcademico { get; set; }
+        public string sTutor { get; set; }
+
+        public string sUsuario { get; set; }
+        public string sServerPath { get; set; }
+        public string sCiclo { get; set; }
+        public string sBeca { get; set; }
+        public string sFormaPago { get; set; }
+    }
+}
